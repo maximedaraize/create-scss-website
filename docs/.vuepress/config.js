897f3c851cca8cb223a4f0f688945595fe680196
Maxime <maxime.daraize@gmail.com>
2021-09-19 10:48:48 -0400

    add config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  lang: "en-US",
+  title: "CREATE SCSS CLI",
+  description: "Official guide of create scss cli",
+
+  themeConfig: {
+    head: [
+      [
+        "link",
+        {
+          rel: "icon",
+          href: "/favicon.ico",
+        },
+      ],
+    ],
+    logo: "https://res.cloudinary.com/mdaraize/image/upload/v1590871168/favicon.svg",
+    navbar: [
+      // NavbarItem
+      {
+        text: "Github",
+        link: "https://github.com/maximedaraize/create-scss-cli",
+      },
+      {
+        text: "Npm",
+        link: "https://www.npmjs.com/package/create-scss-cli",
+      },
+    ],
+  },
+};
